4b0e0387223a344213f311f5a948d04906140eda
Cassi Gallagher <ccgall@gmail.com>
2017-09-29 10:05:42 -0700

    refactored code

--- 01-JS-Drum/js/main.js
+++ 01-JS-Drum/js/main.js
@@ -1,14 +1,34 @@
-window.addEventListener('keydown', function(event){
+// Refactored Solution
+
+function playDrum(event) {
   const audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
   const key = document.querySelector(`div[data-key='${event.keyCode}']`);
-  if(!audio) return;
-
+  if(!audio) return; // shorthand if/else
   audio.currentTime = 0 // this rewinds the audio to the start so the program won't wait until it's done if you hit the key before that.
   audio.play();
   key.classList.add('playing')
-});
+};
 
-// My original solution
+function removeTransition(event) {
+  if(event.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+window.addEventListener('keydown', playDrum);
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+// Original solution
+// window.addEventListener('keydown', function(event){
+//   const audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
+//   const key = document.querySelector(`div[data-key='${event.keyCode}']`);
+//   if(!audio) return;
+//
+//   audio.currentTime = 0 // this rewinds the audio to the start so the program won't wait until it's done if you hit the key before that.
+//   audio.play();
+//   key.classList.add('playing')
+// });
 
 // window.addEventListener('keyup', function(event){
 //   const key = document.querySelector(`div[data-key='${event.keyCode}']`);
@@ -18,12 +38,3 @@ window.addEventListener('keydown', function(event){
 //     key.classList.remove('playing')
 //   };
 // })
-
-// Refactored solution
-function removeTransition(event) {
-  if(event.propertyName !== 'transform') return;
-  this.classList.remove('playing');
-}
-
-const keys = document.querySelectorAll('.key')
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
