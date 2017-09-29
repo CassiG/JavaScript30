a5505c86a72b28a1d78944e37ba8f326bc4d078c
Cassi Gallagher <ccgall@gmail.com>
2017-09-29 09:58:21 -0700

    refactored javascript for removing transition

--- 01-JS-Drum/js/main.js
+++ 01-JS-Drum/js/main.js
@@ -1,20 +1,29 @@
 window.addEventListener('keydown', function(event){
   const audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
   const key = document.querySelector(`div[data-key='${event.keyCode}']`);
-  if(!audio) {
-    return;
-  } else {
-    audio.currentTime = 0 // this rewinds the audio to the start so the program won't wait until it's done if you hit the key before that.
-    audio.play();
-    key.classList.add('playing')
-  };
+  if(!audio) return;
+
+  audio.currentTime = 0 // this rewinds the audio to the start so the program won't wait until it's done if you hit the key before that.
+  audio.play();
+  key.classList.add('playing')
 });
 
-window.addEventListener('keyup', function(event){
-  const key = document.querySelector(`div[data-key='${event.keyCode}']`);
-  if(!key) {
-    return;
-  } else {
-    key.classList.remove('playing')
-  };
-})
+// My original solution
+
+// window.addEventListener('keyup', function(event){
+//   const key = document.querySelector(`div[data-key='${event.keyCode}']`);
+//   if(!key) {
+//     return;
+//   } else {
+//     key.classList.remove('playing')
+//   };
+// })
+
+// Refactored solution
+function removeTransition(event) {
+  if(event.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
